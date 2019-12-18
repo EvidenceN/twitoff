cac73be2ad833cc2faaf4d7890c21838dd53bb42
EvidenceN <33920231+EvidenceN@users.noreply.github.com>
2019-12-17 17:00:51 -0800

    added for loop list to html

--- Twitoff/app.py
+++ Twitoff/app.py
@@ -3,7 +3,7 @@
 # convention is to put imports in alphabetical order
 from decouple import config
 from flask import Flask, render_template, request
-from .models import DB
+from .models import DB, User
 
 # make our app factory
 
@@ -12,11 +12,14 @@ def create_app():
 
     #add config for database
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
     # have the database know about the app
     DB.init_app(app)
+
     # make route
     @app.route('/')
     def root():
-        return render_template('base.html')
+        users = User.query.all()
+        return render_template('base.html', title="Home", users = users)
 
     return app
